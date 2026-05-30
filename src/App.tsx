72d8261bb219ce498754e53aaa62f27bc0118a13
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2026-05-30 12:44:14 -0300

    feat: create scheduling section with title, description and container styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { Flex, Typography } from 'antd'
+import { strings } from './constants/strings'
 import 'antd/dist/reset.css'
-import { ScissorOutlined } from '@ant-design/icons'
 
 
 function App() {
+  
   return (
     <div>
-      <button className='buttonHairDay'>
-        HairDay <ScissorOutlined />
-      </button>
+      <Flex className='containerRight'>
+        <Flex vertical justify="center" style={{ height: '20%', padding: '40px'}}>
+         <Typography.Title 
+            level={3}
+            style={{ color: '#F5F4F5'}}
+          >
+            {strings.scheduling.title}
+          </Typography.Title>
+
+          <Typography.Paragraph
+            style={{ color: '#98959D', maxWidth: 360 }}
+          >
+            {strings.scheduling.description}
+          </Typography.Paragraph>
+        </Flex>
+
+      </Flex>
     </div>
   )
 }
---
+++ src/constants/strings.ts
@@ -0,0 +1,6 @@
+export const strings = {
+  scheduling: {
+    title: 'Agende um atendimento',
+    description: 'Selecione data, horário e informe o nome do cliente para criar o agendamento',
+  },
+}
