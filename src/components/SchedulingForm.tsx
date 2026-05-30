089154359622146d2c13e07955ff9ede29764a62
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2026-05-30 16:38:26 -0300

    feat: refactor SchedulingForm to use inline styles instead of external CSS file

--- src/components/SchedulingForm.tsx
+++ src/components/SchedulingForm.tsx
@@ -1,9 +1,30 @@
+import React from 'react'
 import { Button, DatePicker, Form, Input } from "antd";
 import { CalendarOutlined, DownOutlined, UserOutlined } from '@ant-design/icons'
 import { strings } from '../constants/strings'
 import dayjs, { Dayjs } from 'dayjs'
 
-import './SchedulingForm.css'
+const fieldStyle: React.CSSProperties = {
+    width: '100%',
+    backgroundColor: 'transparent',
+    border: '1px solid var(--color-border)',
+    padding: '13px',
+    color: 'var(--color-text-secondary)',
+}
+
+const buttonStyle: React.CSSProperties = {
+    width: '100%',
+    backgroundColor: 'var(--color-primary)',
+    border: 'none',
+    padding: '30px',
+    color: 'black',
+    fontWeight: '600'
+}
+
+const iconsStyle: React.CSSProperties = {
+    color: 'var(--color-primary)', 
+    fontSize: '20px'
+}
 
 
 export function SchedulingForm() {
@@ -17,9 +38,9 @@ export function SchedulingForm() {
             >
                 <DatePicker
                     placeholder={strings.scheduling.datePlaceholder}
-                    className="scheduling-field"
+                    style={fieldStyle}
                     size="large"
-                    prefix={<CalendarOutlined className="scheduling-icon" />}
+                    prefix={<CalendarOutlined style={ iconsStyle } />}
                     suffixIcon={<DownOutlined style={{ color: 'var(--color-text-secondary)' }} />}
                     format="DD/MM/YYYY"
                     disabledDate={disabledData}
@@ -32,14 +53,14 @@ export function SchedulingForm() {
             >
                 <Input 
                     placeholder={strings.scheduling.clientPlaceholder} 
-                    className="scheduling-field"
-                    prefix={<UserOutlined className="scheduling-icon" />}
+                    style={fieldStyle}
+                    prefix={<UserOutlined style={ iconsStyle } />}
                     size="large"
                 />
             </Form.Item>
 
             <Button
-               className="scheduling-button"
+               style={buttonStyle}
             >
                 {strings.scheduling.submitButton}
             </Button>
