fb2519f0960bd9e3cc143e6d4f3b95f4bcd1fdbb
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2026-05-30 15:24:53 -0300

    feat: add date picker label and placeholder strings to scheduling constants

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -2,5 +2,7 @@ export const strings = {
   scheduling: {
     title: 'Agende um atendimento',
     description: 'Selecione data, horário e informe o nome do cliente para criar o agendamento',
+    datePlaceholder: 'Selecione a data',
+    dateLabel: 'Data',
   },
 }
